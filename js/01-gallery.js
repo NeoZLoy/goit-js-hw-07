7b54b5a611a7e0c0efeabc813ba7ad23ac3317ed
NeoZLoy <123673700+NeoZLoy@users.noreply.github.com>
2023-06-13 02:47:22 +0200

    tasks completed

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,52 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
+
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+galleryContainer.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+  onGalleryItemClick();
+});
+
+function createGalleryMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+          <a class="gallery__link"  href="${original}">
+              <img
+              class="gallery__image"
+              src="${preview}"
+              data-source="${original}"
+              alt="${description}"
+              />
+          </a>
+      </li>`;
+    })
+    .join("");
+}
+
+function onGalleryItemClick() {
+  const instance = basicLightbox
+    .create(
+      `<img width="1400" height="900" src="${event.target.dataset.source}">`,
+      {
+        onShow: (instance) =>
+          window.addEventListener("keydown", (evt) => {
+            if (evt.key === "Escape") {
+              instance.close();
+            }
+          }),
+      }
+    )
+    .show();
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+onGalleryItemClick();
+
+function createGalleryMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              alt="${description}"
+            />
+          </a>
+        </li>`;
+    })
+    .join("");
+}
+
+function onGalleryItemClick() {
+  const lightbox = new SimpleLightbox(".gallery a", {
+    captionDelay: 250,
+  });
+  gallery.on("show.simplelightbox", function () {});
+}
